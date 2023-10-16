3db5898b4f1a0fbb407cf6a1761122218e03dc45
Aaron Yarborough <aaron@alienjungle.digital>
2023-10-16 18:11:29 +0100

    animate header

--- src/components/header-nav.tsx
+++ src/components/header-nav.tsx
@@ -1,24 +1,67 @@
+"use client";
+
 import Image from "next/image";
 
-import logo from "../../public/logo.svg";
 import { statics } from "@/static";
+import { Variants, motion } from "framer-motion";
+import logo from "../../public/logo.svg";
 
 export default function HeaderNav() {
+  const links: { href: string; text: string }[] = [
+    {
+      href: "#services",
+      text: "Services",
+    },
+    {
+      href: "#work",
+      text: "Work",
+    },
+    {
+      href: "#testimonials",
+      text: "Testimonials",
+    },
+  ];
+
+  const variants: Variants = {
+    hidden: {},
+    visible: {
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { opacity: 0, y: -50 },
+    visible: { opacity: 1, y: 0 },
+  };
+
   return (
     <nav className="flex flex-row justify-between items-center gap-[47px] px-[45px] py-[25px]">
       <Image src={logo} alt="yarbz.digital logo" />
 
-      <div className="flex flex-row gap-[47px] justify-end items-center font-semibold">
-        <a href="#services">Services</a>
-        <a href="#work">Work</a>
-        <a href="#testimonials">Testimonials</a>
-        <a
+      <motion.div
+        className="flex flex-row gap-[47px] justify-end items-center font-semibold"
+        variants={variants}
+        initial="hidden"
+        animate="visible"
+      >
+        {links.map((link) => {
+          return (
+            <motion.a key={link.href} href={link.href} variants={item}>
+              {link.text}
+            </motion.a>
+          );
+        })}
+
+        <motion.a
           className="bg-yd-orange text-yd-white btn"
           href={statics.calendlyUrl}
+          variants={item}
         >
           Get in touch
-        </a>
-      </div>
+        </motion.a>
+      </motion.div>
     </nav>
   );
 }
