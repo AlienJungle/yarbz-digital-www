af86e8f52a16cc01cd171aa65896d19aeea63d35
Aaron Yarborough <aaron@alienjungle.digital>
2023-10-16 23:37:43 +0100

    animations, not found, prose

---
+++ src/app/not-found.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { introVariants } from "@/animations";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <motion.main
+      className="full-screen flex flex-col items-center justify-center"
+      variants={introVariants}
+      initial={"hidden"}
+      animate={"visible"}
+    >
+      <h1 className="text-2xl font-semibold text-yd-orange">Page not found!</h1>
+      <p className="my-6">
+        Sorry, but it looks like this page doesn&apos;t exist.
+      </p>
+      <Link href="/" className="btn bg-yd-dark-blue text-yd-white">
+        Back home
+      </Link>
+    </motion.main>
+  );
+}
---
+++ src/components/footer.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+export default function Footer(): JSX.Element {
+  return (
+    <footer className="grid grid-cols-3 items-center justify-between px-[45px] py-[25px]">
+      <span>© 2024 Yarbz Digital Ltd</span>
+      <ul className="flex flex-row gap-[15px] items-center justify-center">
+        <li>
+          <Link href="/privacy-policy" tabIndex={0}>
+            Privacy Policy
+          </Link>
+        </li>
+        <li>
+          <span>·</span>
+        </li>
+        <li>
+          <Link href="/cookie-policy" tabIndex={0}>
+            Cookie Policy
+          </Link>
+        </li>
+      </ul>
+      <div className="text-right">
+        <a href="#" tabIndex={0}>
+          Back to top
+        </a>
+      </div>
+    </footer>
+  );
+}
