63fb5c7cdaaf86ae07a7fe7cab9184a55409a556
Aaron Yarborough <aaron@alienjungle.digital>
2023-10-26 22:50:33 +0100

    update coming-soon text

--- src/app/(placeholder)/coming-soon/page.tsx
+++ src/app/(placeholder)/coming-soon/page.tsx
@@ -6,7 +6,7 @@ export default function CominSoonPage() {
   return (
     <main className="h-screen flex flex-col items-center justify-center">
       <Image src={logo} alt="Yarbz Digital logo" className="w-[15%]" />
-      <p className="font-semibold">Update coming soon...</p>
+      <p className="font-semibold">A new brand is on its way...</p>
     </main>
   );
 }
